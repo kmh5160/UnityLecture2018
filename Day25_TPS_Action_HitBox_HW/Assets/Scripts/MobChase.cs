c0277a67d0ee6cf80defdac4594d1e7426de0e87
Kang <kmh5160@naver.com>
2018-08-22 14:01:31 +0900

    Day26

---
+++ Day25_TPS_Action_HitBox_HW/Assets/Scripts/MobChase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobChase : StateMachineBehaviour {
+    public float chaseSpeed = 2f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Transform target = animator.GetComponent<MobController>().player;
+        Vector3 dir = target.position - animator.transform.position;
+        animator.transform.rotation = Quaternion.Slerp(animator.transform.rotation, Quaternion.LookRotation(dir), 0.1f);
+
+        if (dir.magnitude > 1f)
+            animator.transform.Translate(0, 0, chaseSpeed * Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
---
+++ Day26_TPS_Action_HitReaction/Assets/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public const int maxHealth = 100;
+    public float currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+    public void DecreaseHP(float amount)
+    {
+        currentHealth -= amount;
+        //currentHealth = Mathf.Clamp01(currentHealth / maxHealth);
+        if (currentHealth <= 0f)
+        {
+            currentHealth = 0f;
+            print("Dead!");
+        }
+        healthBar.localScale = new Vector3(currentHealth / maxHealth, 1f, 1f);
+    }
+
+    public void IncreaseHP(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        healthBar.localScale = new Vector3(currentHealth / maxHealth, 1f, 1f);
+    }
+}
