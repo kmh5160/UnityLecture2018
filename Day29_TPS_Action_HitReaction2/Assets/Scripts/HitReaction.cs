b8afdabe27fb2d97dd46201a9d4840b686045137
Kang <kmh5160@naver.com>
2018-08-27 13:04:11 +0900

    Day29

---
+++ Day29_TPS_Action_HitReaction2/Assets/Scripts/HitReaction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitReaction : MonoBehaviour {
+    public GameObject hitFXPrefab;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(int amount, string reactionType, Vector3 knockBackDir)
+    {
+        if (anim != null)
+            anim.SetTrigger(reactionType);
+        GetComponent<Health>().DecreaseHP(amount);
+        GetComponent<Rigidbody>().velocity = knockBackDir * 2f;
+        GameObject clone = Instantiate(hitFXPrefab, transform.position, Quaternion.LookRotation(-knockBackDir));
+        Destroy(clone, 1.5f);
+    }
+}
