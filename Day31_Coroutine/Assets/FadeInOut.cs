518ff1df63ca1c07db56ca22f1ae7e8a018b8a9c
Kang <kmh5160@naver.com>
2018-08-29 13:47:28 +0900

    Day31

---
+++ Day31_Coroutine/Assets/FadeInOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            StartCoroutine(FadeIn());
+        }		
+	}
+
+    IEnumerator FadeIn()
+    {
+        for(float f = 1.0f; f >= 0; f -= 0.01f)
+        {
+            MeshRenderer renderer = GetComponent<MeshRenderer>();
+            Color c = renderer.material.color;
+            c.a = f;
+            renderer.material.color = c;
+            yield return null;
+        }
+    }
+}
---
+++ Day31_Coroutine/Assets/FadeInOut0.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut0 : MonoBehaviour {
+    float alpha = 1.0f;
+    bool fadeIn = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I) && !fadeIn)
+        {
+            alpha = 1.0f;
+            fadeIn = true;
+        }		
+        if (fadeIn && alpha > 0f)
+        {
+            MeshRenderer renderer = GetComponent<MeshRenderer>();
+            Color c = renderer.material.color;
+            alpha -= 0.01f;
+            if (alpha < 0f)
+            {
+                alpha = 0f;
+                fadeIn = false;
+            }
+            c.a = alpha;
+            renderer.material.color = c;
+        }
+	}
+}
