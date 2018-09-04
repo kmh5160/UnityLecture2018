50da524da0b0d254bd5320231b4c1d11897b5c18
kmh5160 <kmh5160@naver.com>
2018-09-04 19:20:52 +0900

    Day35 HW

---
+++ Day35_UI_Image_Button_HW/Assets/GameMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMgr : MonoBehaviour {
+
+    public Button startButton;
+    public Animator anim;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        print("Clicked!");
+        anim.SetTrigger("Died");
+    }
+}
