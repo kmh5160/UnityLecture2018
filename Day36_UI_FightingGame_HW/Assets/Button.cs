c48e0e55e2425c96cb589ec19e4e885b9cd87b79
kmh5160 <kmh5160@naver.com>
2018-09-05 20:49:47 +0900

    Day36 HW

---
+++ Day36_UI_FightingGame_HW/Assets/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    public GameObject LeftHP;
+    public GameObject RightHP;
+
+	public void OnClick()
+    {
+        print("Click");
+        LeftHP.GetComponent<Image>().fillAmount -= 0.05f;
+        RightHP.GetComponent<Image>().fillAmount -= 0.03f;
+    }
+}
---
+++ Day36_UI_FightingGame_HW/Assets/Timer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    float Second = 99f;
+
+	void Update () {
+        if (Second <= 0)
+            return;
+        Second -= Time.deltaTime;
+        //new WaitForSeconds(1);
+        GetComponentInChildren<Text>().text = ((int)Second).ToString();
+	}
+}
