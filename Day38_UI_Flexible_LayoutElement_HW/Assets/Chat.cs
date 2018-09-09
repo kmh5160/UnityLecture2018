fc73b66fbb9fac88c28b915f4d8a3da9eda5be2e
kmh5160 <kmh5160@naver.com>
2018-09-10 03:17:55 +0900

    Day38

---
+++ Day38_UI_Flexible_LayoutElement_HW/Assets/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour {
+    public GameObject Send;
+    public GameObject target;
+    public GameObject InputText;
+
+    public void OnClick()
+    {
+        GameObject g = Instantiate(Send);
+        g.GetComponentInChildren<Text>().text = InputText.GetComponent<InputField>().text;
+        InputText.GetComponent<InputField>().text = "";
+        g.transform.SetParent(target.transform);
+        InputText.GetComponent<InputField>().Select();
+        InputText.GetComponent<InputField>().ActivateInputField();
+    }
+}
---
+++ Day38_UI_Flexible_LayoutElement_HW/Assets/EnterKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterKey : MonoBehaviour {
+    public GameObject Send;
+    public GameObject target;
+    public GameObject InputText;
+
+    void Update () {
+        //if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            GameObject g = Instantiate(Send);
+            g.GetComponentInChildren<Text>().text = InputText.GetComponent<InputField>().text;
+            InputText.GetComponent<InputField>().text = "";
+            g.transform.SetParent(target.transform);
+            InputText.GetComponent<InputField>().Select();
+            InputText.GetComponent<InputField>().ActivateInputField();
+        }
+    }
+}
