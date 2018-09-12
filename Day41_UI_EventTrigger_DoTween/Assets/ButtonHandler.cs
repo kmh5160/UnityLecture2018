e119bc71f5b8c6bd002f49e87b3209892c8cec7e
Kang <kmh5160@naver.com>
2018-09-12 13:56:22 +0900

    Day41

---
+++ Day41_UI_EventTrigger_DoTween/Assets/ButtonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ButtonHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler {
+
+	public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.DOScale(0.8f, 0.1f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.DOScale(1f, 0.1f);
+    }
+}
