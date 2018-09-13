2a0f5ff5798253ef12f86e9a903badd66dc4c283
Kang <kmh5160@naver.com>
2018-09-13 14:54:29 +0900

    Hero

---
+++ Hero/Assets/Bounce_Grass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce_Grass : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            print("Bounce!");
+            collision.collider.GetComponent<Rigidbody>().AddForce(transform.forward * 3f);
+        }
+    }
+}
