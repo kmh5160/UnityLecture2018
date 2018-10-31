8db2a5121e56372436699b52a4c3a99a3b14a942
Kang <kmh5160@naver.com>
2018-10-31 14:07:42 +0900

    181031

---
+++ Day66_U_Net/Assets/ChatSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatSystem : MonoBehaviour {
+    public RectTransform chatPrefab;
+    public RectTransform content;
+    public InputField inputField;
+
+	// Use this for initialization
+	//void Start () {
+ //       inputField.onEndEdit.AddListener((message) =>
+ //       {
+ //           AddMessage(message, Random.Range(0, 2) == 0 ? true : false);
+ //           inputField.Select();
+ //           inputField.ActivateInputField();
+ //           //inputField.text = string.Empty;
+ //       });
+	//}
+	
+	public void AddMessage(string message, bool isAlignLeft)
+    {
+        if (message.Length > 0)
+        {
+            RectTransform chat = Instantiate(chatPrefab, content);
+            chat.GetComponent<ChatMessage>().SetMessage(message, isAlignLeft);
+        }
+    }
+
+}
---
+++ Day66_U_Net/Assets/Chatting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Chatting : NetworkBehaviour {
+    ChatSystem chatSystem;
+    string nickName;
+
+    private void Start()
+    {
+        chatSystem = GameObject.FindObjectOfType<ChatSystem>();
+        var inputField = chatSystem.inputField;
+        inputField.onEndEdit.AddListener(message =>
+        {
+            if (!isLocalPlayer)
+                return;
+
+            print(message);
+            if (message != string.Empty)
+            {
+                CmdSendChat(nickName + ": " + message);
+            }
+        });
+    }
+
+    [Command]
+    void CmdSendChat(string message)
+    {
+        RpcReceivedChat(message);
+    }
+
+    [ClientRpc]
+    private void RpcReceivedChat(string message)
+    {
+        chatSystem.AddMessage(message, !isLocalPlayer);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        nickName = "User " + netId;
+    }
+
+}
