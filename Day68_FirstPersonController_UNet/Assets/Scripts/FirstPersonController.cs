a41ce39c783e99a8b825f7ce1706daf331869c4d
Kang <kmh5160@naver.com>
2018-11-02 16:19:08 +0900

    181102

---
+++ Day68_FirstPersonController_UNet/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FirstPersonController : NetworkBehaviour {
+    public float moveSpeed = 8f;
+    public float jumpForce = 250f;
+    public float mouseSensitivityX = 2f;
+    public float mouseSensitivityY = 2f;
+    public GameObject healFX;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    Rigidbody rb;
+    bool isHealing = false;
+    private bool grounded = false;
+    private GameObject fx;
+
+    Transform cameraTransform;
+    float verticalLookRotation;
+
+    private void Awake()
+    {
+        GetComponentInChildren<Camera>().enabled = false;
+        GetComponentInChildren<AudioListener>().enabled = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.magenta;      
+        cameraTransform = GetComponentInChildren<Camera>().transform;
+        GetComponentInChildren<Camera>().enabled = true;
+        GetComponentInChildren<AudioListener>().enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Use this for initialization
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    cameraTransform = GetComponentInChildren<Camera>().transform;
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = false;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1 + 0.1f))
+            grounded = true;
+        else
+            grounded = false;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            //rb.velocity = false;
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        moveDirection = (new Vector3(h, 0, v)).normalized;
+        moveDirection *= moveSpeed;
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -30, 30);
+        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+                
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = transform.TransformDirection(moveDirection) * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "Step3")
+        {
+            if (!isHealing)
+            {
+                fx = Instantiate(healFX, transform.Find("FXPos"));
+                isHealing = true;
+                Invoke("RemoveHealFX", 1.9f);
+            }
+        }
+    }
+
+    void RemoveHealFX()
+    {
+        Destroy(fx);
+        isHealing = false;
+    }
+
+}
---
+++ Day68_FirstPersonController_UNet/Assets/Scripts/Gun.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Gun : NetworkBehaviour {
+    public GameObject weapon;
+    public GameObject shellPrefab;
+    public Transform shellEjection;
+    public float fireRate = 10;
+    public Light muzzleFlash;
+    public GameObject impactFX;
+    public GameObject bulletHolePrefab;
+
+    Camera fpsCamera;
+    float nextTimeToFire = 0f;
+    Vector3 originPos;
+    Vector3 smoothVel;
+
+    private void Awake()
+    {
+        fpsCamera = GetComponentInChildren<Camera>();
+    }
+
+    private void Start()
+    {
+        originPos = weapon.transform.localPosition;
+    }
+
+    void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+
+		if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1 / fireRate;
+            CmdShoot();
+        }
+
+        
+	}
+
+    [Command]
+    private void CmdShoot()
+    {
+        muzzleFlash.enabled = true;
+        Invoke("OffMuzzleFlash", 0.05f);
+        MakeShell();
+
+        CheckHit();
+
+        //Shooting Sound
+        GetComponent<AudioSource>().Play();
+
+        //GameObject fx = Instantiate(impactFX, hit.point, Quaternion.identity);
+        //Destroy(fx, 0.3f);
+
+        //MakeBulletHole(hit.point, hit.normal, hit.transform);
+
+        weapon.transform.localPosition -= Vector3.forward * UnityEngine.Random.Range(0.07f, 0.3f);
+    }
+
+    void CheckHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, 200f))
+        {
+            //print(hit.transform.name);
+            NetworkIdentity ni = hit.transform.gameObject.GetComponentInParent<NetworkIdentity>();
+            if (ni != null)
+                RpcHitReaction(ni.netId, hit.point, hit.normal);
+        }
+    }
+
+    [ClientRpc]
+    private void RpcHitReaction(NetworkInstanceId id, Vector3 point, Vector3 normal)
+    {
+        GameObject hit = ClientScene.FindLocalObject(id);
+        Rigidbody rb = hit.GetComponent<Rigidbody>();
+        if (rb != null)
+            rb.AddForce(fpsCamera.transform.forward * 500f);
+        //hit.transform.GetComponent<AudioSource>().Play();
+        BulletSound bs = hit.transform.GetComponent<BulletSound>();
+        if (bs != null)
+            bs.Play();
+    }
+
+    private void MakeBulletHole(Vector3 point, Vector3 normal, Transform parent)
+    {
+        GameObject clone = Instantiate(bulletHolePrefab, point + normal * 0.02f, Quaternion.FromToRotation(-Vector3.forward, normal), parent);
+        clone.transform.SetParent(parent, true);
+        Destroy(clone, 3f);
+    }
+
+    void OffMuzzleFlash()
+    {
+        muzzleFlash.enabled = false;
+    }
+
+    private void MakeShell()
+    {
+        GameObject clone = Instantiate(shellPrefab, shellEjection);
+        //clone.transform.parent = null;
+        clone.transform.SetParent(null);
+    }
+
+    private void LateUpdate()
+    {
+        weapon.transform.localPosition = Vector3.SmoothDamp(weapon.transform.localPosition, originPos, ref smoothVel, 0.1f);
+    }
+}
